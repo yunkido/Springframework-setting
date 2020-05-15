5b9dd9085d8120f4db70f8f8365c6c4a0979476d
As a person who dreams of new challenges, I always want to show a new look <64242442+yunkido@users.noreply.github.com>
2020-05-15 17:00:25 +0900

    Spring Maven project 기본셋팅파일
    이제부터 시작입니다.
    많은 응원 부탁드립니다.

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+package kr.co.softcampus.beans;
+
+public interface HelloWorld {
+	
+	public void sayHello();
+
+}
---
+++ HelloWorldEn.java
@@ -0,0 +1,12 @@
+package kr.co.softcampus.beans;
+
+public class HelloWorldEn implements HelloWorld{
+	@Override
+	public void sayHello() {
+		// TODO Auto-generated method stub
+		System.out.println("HI~~");
+		
+	}
+	
+}
+ 
